61c784e6e1e2035744d2c3683fd11ca5fb60bfab
hosung <hslee1258@gmail.com>
2022-09-20 17:06:01 +0900

    「Bootstrap tag」add

--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import App from './App.vue'
 import router from "./router/index"
 import BootstrapVue3 from 'bootstrap-vue-3'
 
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 
 createApp(App).use(BootstrapVue3).use(router).mount('#app')
