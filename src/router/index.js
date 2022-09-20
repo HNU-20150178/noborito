0876ed05e6d4f62b6c4822c5efb5d1890433d06a
hosung <hslee1258@gmail.com>
2022-09-20 16:27:57 +0900

    「router」add

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/PGF0001',
+    name: 'PGF0001',
+    component: () => import('../views/Pgf00/PGF0001')
   }
 ]
 
