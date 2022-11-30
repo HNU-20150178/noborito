1be1225bc14384a6a0f61e3827d404fa437148c1
HONGCHEOL <yhc9632@gmail.com>
2022-11-30 10:29:28 +0900

    router modify

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Index</router-link> |
+    <router-link to="/work">Work</router-link> |
+    <router-link to="/contact">Contact</router-link> |
+    <router-link to="/PGF0001">PGF0001</router-link>
   <router-view/>
   </nav>
 </template>
--- src/views/Pgf00/PGF0001.vue
+++ src/views/Pgf00/PGF0001.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
+  <!-- https://dunks1980.com/   https://www.hostinger.com/tutorials/web-developer-portfolio https://www.mygreatlearning.com/blog/web-development-project-ideas/ -->
     <h1>This is an 0001 page</h1>
   </div>
 </template>
